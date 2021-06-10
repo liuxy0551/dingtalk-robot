aa1ecc1d0a8f8c1a7cca18bb559307dde0ec06f5
liuyi <liuyi@dtstack.com>
2021-06-10 17:04:09 +0800

    update baidutj msg

--- app/service/send.js
+++ app/service/send.js
@@ -13,10 +13,12 @@ class SendService extends Service {
 
       const text = `【今日】\n- PV：${ today[0] }\n- UV：${ today[1] }\n- IP数：${ today[2] }\n- 平均访问时长：${ getTimeStr(today[3]) }\n\n【昨日】\n- PV：${ yesterday[0] }\n- UV：${ yesterday[1] }\n- IP数：${ yesterday[2] }\n- 平均访问时长：${ getTimeStr(yesterday[3]) }`
       const msg = {
-        msgtype: 'markdown',
-        markdown: {
+        msgtype: 'actionCard',
+        actionCard: {
           title: '百度统计 - 网站数据',
-          text
+          text,
+          singleTitle: '点此查看更多数据',
+          singleURL: `https://tongji.baidu.com/m/#/report/${config.body.siteId}`
         }
       }
 
